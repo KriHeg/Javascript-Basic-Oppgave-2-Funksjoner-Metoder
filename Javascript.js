9ae7ab64484f4d483aa1a50c48891396642f35b8
KriHeg <kristian.1.kodehode@gmail.com>
2026-05-11 10:58:32 +0200

    first commit

---
+++ Javascript.js
@@ -0,0 +1,243 @@
+/******************************************************************************
+Funksjoner og metoder oppgave
+
+Les oppgaveteksten NØYE. Vis noen eksempler i koden din som tester
+funksjonene og metodene dine. Bruk en variasjon av pilfunksjoner (arrow functions)
+og funksjoner laget med nøkkelordet `function`.
+
+Legg til kommentarer i koden din som kort forklarer hva den gjør.
+
+******************************************************************************/
+
+/******************************************************************************
+1.
+
+Lag følgende funksjon:
+
+Funksjonen skal ta inn et tall som parameter og returnere
+"Oddetall" hvis tallet er et oddetall og "Partall" hvis tallet er et partall.
+(PS: Funksjonen skal bruke return, du skal ikke bruke console log inni
+funksjonen)
+
+******************************************************************************/
+
+// Skriv koden for oppgave 1 her
+function checkNumber(number) {
+  if (number % 2 === 0) {
+    return "Partall";
+  } else {
+    return "Oddetall";
+  }
+}
+checkNumber(2);
+
+/******************************************************************************
+2.
+
+Lag følgende funksjon:
+
+Funksjonen skal ta inn en string som parameter og returnere stringen
+i STORE BOKSTAVER med et utropstegn på slutten.
+
+Eksempel: "Dette er kult" skal returnere "DETTE ER KULT!"
+
+******************************************************************************/
+
+// Skriv koden for oppgave 2 her
+const makeUpperCaps = (sentence) => {
+  let coolsentence = sentence.toUpperCase() + "!";
+  return coolsentence;
+};
+
+makeUpperCaps("Dette er kult");
+
+/******************************************************************************
+3.
+
+Lag følgende funksjon:
+
+Funksjonen skal ta inn 2 parametere:
+
+ - Et navn (string)
+ - En time på døgnet (nummer)
+
+Funksjonen skal returnere:
+"Ugyldig tid" hvis timeverdien er mindre enn 0.
+"God natt (mottatt navn)" hvis timeverdien er mellom 0 og 5.
+"God morgen (mottatt navn)" hvis timeverdien er mellom 6 og 11.
+"God dag (mottatt navn)" hvis timeverdien er mellom 12 og 17.
+"God kveld (mottatt navn)" hvis timeverdien er mellom 18 og 23.
+"Ugyldig tid" hvis timeverdien er større enn 23.
+
+Hvis ingen timeverdi mottas, skal funksjonen returnere en feilmelding.
+
+******************************************************************************/
+
+// Skriv koden for oppgave 3 her
+function dayTimeGreeter(name, number) {
+  let message = "";
+  if (number === 0 || number > 23) {
+    message = "Ugyldig tid";
+  } else if (number > 0 && number < 6) {
+    message = "god natt" + " " + name;
+  } else if (number > 5 && number < 12) {
+    message = "God morgen" + " " + name;
+  } else if (number > 11 && number < 18) {
+    message = "God dag" + " " + name;
+  } else if (number > 17 && number < 24) {
+    message = "God kveld" + " " + name;
+  } else {
+    message = "du har ikke satt en timeverdi";
+  }
+  return message;
+}
+dayTimeGreeter("Per", 23);
+
+/******************************************************************************
+4.
+
+Lag følgende funksjon:
+
+Funksjonen skal ta inn en array som parameter og returnere arrayen
+med første og siste indeks fjernet.
+
+Eksempel 1: ["Rød", "Grønn", "Blå", "Gul"] skal returnere ["Grønn", "Blå"].
+
+Eksempel 2: ["En", "To", "Tre", "Fire", "Fem", "Seks"] skal returnere
+["To", "Tre", "Fire", "Fem"].
+
+******************************************************************************/
+
+// Skriv koden for oppgave 4 her
+
+function cutArray(list) {
+  list.shift();
+  list.pop();
+  const shownArray = list;
+  return list;
+}
+cutArray(["rød", "Grønn", "Blå", "Gul"]);
+cutArray(["En", "To", "Tre", "Fire", "Fem", "Seks"]);
+
+/******************************************************************************
+5.
+
+Lag følgende funksjon:
+
+Funksjonen skal ta inn en string som parameter.
+
+Bruk stringmetoder på stringen for å gjøre følgende:
+ - Erstatt ordet "vanskelig" med "gøy".
+ - Fjern mellomrom fra starten og slutten av stringen.
+
+Returner deretter den oppdaterte stringen.
+
+Eksempel 1: "  Javascript er vanskelig   " skal returnere "Javascript er gøy".
+Eksempel 2: " Det er vanskelig å bruke metoder " skal returnere "Det er gøy å bruke metoder".
+Eksempel 3: "   vanskelig        " skal returnere "gøy".
+
+******************************************************************************/
+
+// Skriv koden for oppgave 5 her
+function replaceText(text) {
+  let message = text.replaceAll("vanskelig", "gøy");
+  message = message.trim();
+  return message;
+}
+replaceText("  Javascript er vanskelig   ");
+replaceText(" Det er vanskelig å bruke metoder ");
+replaceText("   vanskelig        ");
+
+/******************************************************************************
+6.
+
+Fullfør følgende steg for å manipulere "items"-arrayet. Hvert steg skal
+fullføres ved å bruke passende array-metoder.
+
+*******************************************************************************/
+
+const items = ["Bok", "Penn", "Notatbok", "Viskelær", "Blyant", "Markør"];
+
+/*******************************************************************************
+Steg 1: Fjern det første elementet ("Bok") fra arrayen ved hjelp av riktig metode.
+
+Steg 2: Finn og erstatt "Viskelær" med "Linjal" i arrayen.
+
+Steg 3: Bruk splice-metoden til å fjerne både "Penn" og "Notatbok", og legg til "Markeringspenn" i deres plass.
+
+Steg 4: Kombiner alle elementene i arrayen til en enkelt string ved å bruke " | " som separator.
+
+Ekstra utfordring: Lag et nytt array som kun inkluderer elementer som inneholder bokstaven "e".
+
+******************************************************************************/
+
+// Skriv koden for oppgave 6 her
+items.shift();
+console.log(items);
+
+let index = items.indexOf("Viskelær");
+items.splice(index, 1, "Linjal");
+console.log(items);
+
+items.splice(0, 2, "Markeringspenn");
+console.log(items);
+
+let arrayToString = items.join("|");
+console.log(arrayToString);
+
+const onlye = items.filter((item) => item.includes("e"));
+console.log(onlye);
+
+/******************************************************************************
+7.
+
+EKSTRA UTFORDRING #1:
+
+Dette er ikke obligatorisk, kun for de som vil ha en ekstra utfordring.
+
+Lag følgende funksjon:
+
+Funksjonen skal ta inn 2 parametere, en array og en string.
+
+Sjekk om arrayen inneholder stringen. Hvis den gjør det, fjern elementet
+fra arrayet og returner den oppdaterte arrayen.
+
+Hvis arrayet ikke inneholder stringen, legg stringen til på slutten
+av arrayet og returner det oppdaterte arrayet.
+
+Eksempel 1: (["Rød", "Grønn"], "Blå") --> ["Rød", "Grønn", "Blå"]
+Eksempel 2: (["Rød", "Grønn", "Blå"], "Grønn") --> ["Rød", "Blå"]
+Eksempel 3: (["En", "To", "Tre"], "Fire") --> ["En", "To", "Tre", "Fire"]
+Eksempel 4: (["En", "To", "Tre"], "To") --> ["En", "Tre"]
+
+******************************************************************************/
+
+// Skriv koden for oppgave 7 her
+
+/******************************************************************************
+8.
+
+EKSTRA UTFORDRING #2:
+
+Dette er ikke obligatorisk, kun for de som vil ha en ekstra utfordring.
+
+Lag følgende funksjon:
+
+Funksjonen skal ta inn ett parameter.
+
+Hvis parameteret er en string:
+Returner stringen med "😎" lagt til i starten og slutten.
+
+Hvis parameteret er et tall:
+Doble verdien, konverter den til en string, og returner den med "😎" lagt til i
+starten og slutten.
+
+Hvis parameteret er en boolean:
+Returner "😎Ja😎" hvis parameteret er true, eller "😎Slapp av😎" hvis parameteret er false.
+
+Hvis parameteret er en annen datatype:
+Returner "😎Kun primitive verdier😎".
+
+******************************************************************************/
+
+// Skriv koden for oppgave 8 her
